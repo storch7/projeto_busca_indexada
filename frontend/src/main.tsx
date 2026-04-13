858e56f122954f03e073a793c7d4ea80f2357a85
Guilherme Storch de Oliveira <GSOliveira@brasal.com.br>
2026-04-12 21:03:03 -0300

    Organização da estrutura do arquivo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,3 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+// Ambiente voltado apenas a testes lógicos
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+console.log("=== Motor de Busca Indexada Iniciado ===");
